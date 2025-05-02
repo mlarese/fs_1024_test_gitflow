88a64ab2d16632455f96cf4f606e723355b62727
mauro_larese <mauro.larese@omnys.com>
2025-05-02 10:04:04 +0200

    Initial commit

---
+++ src/main/java/it/epicode/test_gitflow/TestGitflowApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.test_gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/test_gitflow/TestGitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.test_gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
