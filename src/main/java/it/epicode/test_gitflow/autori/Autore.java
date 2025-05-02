6a40d6210b26687b7d2d476ffc00351b3a876b68
mauro_larese <mauro.larese@omnys.com>
2025-05-02 10:17:17 +0200

    add Autore class with properties for name, surname, email, phone, address, city, province, postal code, and country

---
+++ src/main/java/it/epicode/test_gitflow/autori/Autore.java
@@ -0,0 +1,16 @@
+package it.epicode.test_gitflow.autori;
+
+import lombok.Data;
+
+@Data
+public class Autore {
+    private String nome;
+    private String cognome;
+    private String email;
+    private String telefono;
+    private String indirizzo;
+    private String citta;
+    private String provincia;
+    private String cap;
+    private String nazione;
+}
